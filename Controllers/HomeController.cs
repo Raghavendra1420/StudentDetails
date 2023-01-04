ae1aa962eadf5f584fe8314cee7929cff9b3b280
rp208 <rp208@US-5CG9524V7D>
2023-01-04 11:46:11 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StudentDetails.Models;
+
+namespace StudentDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        private tblStudentInfoEntities1 db = new tblStudentInfoEntities1();
+
+        // GET: StudentDetails
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.StudentDetails.ToListAsync());
+        }
+
+        // GET: StudentDetails/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = await db.StudentDetails.FindAsync(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // GET: StudentDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "StudentId,StudentName,StudentMobile,StudentAddress,StudentDept")] StudentDetail studentDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentDetails.Add(studentDetail);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentDetail);
+        }
+
+        // GET: StudentDetails/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = await db.StudentDetails.FindAsync(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // POST: StudentDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "StudentId,StudentName,StudentMobile,StudentAddress,StudentDept")] StudentDetail studentDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentDetail).State = (System.Data.Entity.EntityState)System.Data.EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(studentDetail);
+        }
+
+        // GET: StudentDetails/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentDetail studentDetail = await db.StudentDetails.FindAsync(id);
+            if (studentDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetail);
+        }
+
+        // POST: StudentDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            StudentDetail studentDetail = await db.StudentDetails.FindAsync(id);
+            db.StudentDetails.Remove(studentDetail);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
